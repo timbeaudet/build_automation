a165a96b5a84746fce045a4fb49d35d330ffbcb6
Tim Beaudet <timbeaudet.yahoo.com>
2017-10-20 05:38:34 -0700

    Looping over scripts in automated directories, getting return value for success/fail

--- automate_builds.sh
+++ automate_builds.sh
@@ -1,25 +1,51 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #
 # Automate Builds is a script to recursively search through a directory for automated build scripts and call each
 #   in the specified order. Future revisions may allow the called script to return a value to continue or cancel the
 #   calling of the remaining files.
 #
-#---------------------------------------------------------------------------------------------------------------------#
+# <!-- Copyright (c) 2017 Tim Beaudet - All Rights Reserved -->
+# ---------------------------------------------------------------------------------------------------------------------
 
-build_scripts=("auto_update.sh" "auto_clean.sh" "auto_build.sh" "auto_test.sh" "auto_deploy.sh")
+# This is the set/list of scripts that will be called in the order given, left to right.
+declare -a build_scripts=("auto_update.sh" "auto_clean.sh" "auto_build.sh" "auto_test.sh" "auto_deploy.sh")
 
-echo "Starting auto run from: $(pwd)"
+auto_build_setting_initial_directory=`pwd`
+echo "$auto_build_setting_initial_directory"
 
-for d in ./ */ **/*; do
-    for f in "${build_scripts[@]}" ; do
-        if [ -f "$d/$f" ]; then
-            export CurrentDirectory=$(pwd)
-            cd $d
-            sh "$f"
-            cd $CurrentDirectory
-        fi
-    done
+found_script=0
+abs_return_value=0
+for d in `find . -type d -path ./"*automated"`; do
+	#We already know we are in an automated directory, no need to test.
+	#This does skip the edge-case that the automated_builds script was run from a directory named 'automated'
+	if [[ -d "$d" ]]; then
+		found_script=0
+		abs_return_value=0
+		pushd "$d"		
+   		for f in "${build_scripts[@]}" ; do
+   			if [[ "$abs_return_value" -eq 0 ]]; then
+    	    	if [ -f "$f" ]; then
+   	    			found_script=1
+       	    		. "$f"
+
+#      	    		IF 0==!abs_return_value! (
+#						REM Continue on to the next build phase.
+#					) ELSE (
+#						(ECHO FAILED: "!pathLocalToCurrent!\%%f"   ERROR: !abs_return_value! [stopping current project])>>!abs_summary_report_file!
+#					)
+
+					if [[ "$abs_return_value" -eq 0 ]]; then
+						# Continue on to the next build phase.
+						echo pass > /dev/null
+					else
+						echo fail
+					fi
+
+        		fi
+        	fi
+   		done
+    	popd > /dev/null
+    fi # -d loop
 done
 
-exit 1
