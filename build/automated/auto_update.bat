ab142647c3c724c1bfc35c565489ffc3474eeaf8
Tim Beaudet <timbeaudet@yahoo.com>
2017-10-25 06:41:20 -0400

    Windows update script updated to print to file, and check if abs_detailed_report was defined.

--- build/automated/auto_update.bat
+++ build/automated/auto_update.bat
@@ -1,7 +1,7 @@
 @ECHO off
 
 REM
-REM Automated Build Script for TEMPLATE_PROJECT_NAME to automate the update process by grabbing the latest from the source
+REM Automated Build Script to automate the update process by grabbing the latest from the source
 REM control repository, used here is subversion (svn) though it shouldn't be hard to modify to Git or Hg.
 REM
 REM This may be run by an automated process to clean and/or build each project with an /automated/auto_ script.
@@ -10,6 +10,17 @@ REM Available on github: https://www.github.com/timbeaudet/build_automation/ und
 REM -------------------------------------------------------------------------------------------------------------------
 
 REM This should go back to project_root to update, which by default was 2 levels back from: project_root/build/automated/
+
+IF NOT DEFINED abs_detailed_report_file (
+	ECHO WARNING: abs_detailed_report_file was not set.
+	SET abs_detailed_report_file="auto_build_report.txt"
+)
+
+(ECHO.)>>%abs_detailed_report_file%
+(ECHO.)>>%abs_detailed_report_file%
+(ECHO updating from source control of %CD%)>>%abs_detailed_report_file%
+(ECHO ========================================================)>>%abs_detailed_report_file%
+
 PUSHD ..\..\
-svn update
+(svn update --quiet --non-interactive)>>%abs_detailed_report_file%
 POPD
