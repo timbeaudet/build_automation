638d0e1a7bcc3d81173017cec1e3318ce68aeb33
Tim Beaudet <timbeaudet@yahoo.com>
2018-11-01 10:11:19 -0400

    Update auto_clean.sh

--- build/automated/auto_clean.sh
+++ build/automated/auto_clean.sh
@@ -5,7 +5,7 @@
 #
 # This may be run by an automated process to clean and/or build each project with an /automated/auto_ script.
 #
-# Available on github: https://www.github.com/timbeaudet/build_automation/ under the unliscense agreement.
+# Available on github: https://www.github.com/timbeaudet/build_automation/ under the unlicense agreement.
 #---------------------------------------------------------------------------------------------------------------------#
 
 pushd ../
@@ -15,13 +15,12 @@ premake5 --file="TEMPLATE_PROJECT_FILE.lua" clean
 kLinuxPlatform="Linux"
 currentPlatform=`uname`
 if [ $kLinuxPlatform == $currentPlatform ]; then
-  #If any deployable gets created, delete it during cleaning.
-  echo blah > /dev/null
+	: #No operation, if any deployable gets created, delete it during cleaning.
 else
-  #If the Mac OS X delpoyable application package exists, delete it during project cleaning.
-  if [ -d "${toRunDir}../TEMPLATE_PROJECT_NAME.app" ]; then
-	rm -r "${toRunDir}../TEMPLATE_PROJECT_NAME.app"
-  fi
+	#If the Mac OS X delpoyable application package exists, delete it during project cleaning.
+	if [ -d "${toRunDir}../TEMPLATE_PROJECT_NAME.app" ]; then
+		rm -r "${toRunDir}../TEMPLATE_PROJECT_NAME.app"
+ 	fi
 fi
 
 popd
