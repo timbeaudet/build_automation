843350545de7ab4676e8ce653bfabe5c9ed016a5
Tim Beaudet <tim@tyrebytes.com>
2024-08-02 09:52:48 -0400

    ensure public config flag is defined.

--- batch/automate_builds.bat
+++ batch/automate_builds.bat
@@ -78,6 +78,7 @@ FOR /r /d %%d IN (*) DO (
 		SET abs_build_version_minor=0
 		SET abs_build_version_revision=0
 		SET abs_skip_if_no_updates=0
+		SET abs_build_public_config=0
 
 		REM This is a great idea, but has two major issues to dig deeper into:
 		REM 1. The config is used for both batch and bash scripts so the .bat vs .sh can't be added to script file.
